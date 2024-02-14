380af40ede98d493c7eb96b570b479ce198dfe6c
zafarzhon <odilovzafarzhon@gmail.com>
2024-02-14 16:12:40 +0300

    create base project

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Zafarzhon_Odilov
+ * @link https://github.com/zafarzhon
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, TMS!!!");
+    }
+}
