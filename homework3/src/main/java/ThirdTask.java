79b43d7c51f5327ed436ce9968325dcb8dd058e6
zafarzhon <odilovzafarzhon@gmail.com>
2024-02-17 01:57:01 +0300

    finish third task

--- homework3/src/main/java/ThirdTask.java
+++ homework3/src/main/java/ThirdTask.java
@@ -13,11 +13,11 @@ public class ThirdTask {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        if(n<=0){
+        if (n <= 0) {
             System.err.println("n must greater than 0!!!");
-        }else{
-            for(int i = 0;i<n;i++){
-                if(i%3==0 && i%2==1){
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (i % 3 == 0 && i % 2 == 1) {
                     System.out.println(i);
                 }
             }
