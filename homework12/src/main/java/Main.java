a694e6d3a01f0c43c9ef1e7cbdbee33e051367e6
zafarzhon <odilovzafarzhon@gmail.com>
2024-03-20 16:02:07 +0300

    add main class and title homework

---
+++ homework12/src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Zafarzhon Odilov
+ * @link <a href="https://github.com/zafarzhon">github.com/zafarzhon</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
