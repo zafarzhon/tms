249583db3f8d59a093fae867fae9fa82d14d90eb
zafarzhon <odilovzafarzhon@gmail.com>
2024-03-21 03:40:10 +0300

    finish homework12

--- homework12/src/main/java/Main.java
+++ homework12/src/main/java/Main.java
@@ -1,9 +1,16 @@
+import Service.ValidateService;
+
 /**
  * @author Zafarzhon Odilov
  * @link <a href="https://github.com/zafarzhon">github.com/zafarzhon</a>
  */
 public class Main {
     public static void main(String[] args) {
-        
+        boolean isValid = ValidateService.validateLoginAndPassword("admin",
+                "admin1",
+                "admin1");
+
+        System.out.println(isValid);
+
     }
 }
