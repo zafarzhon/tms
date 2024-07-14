818b5a248673783c9f5dbd61ead11fe632c33858
zafarzhon <odilovzafarzhon@gmail.com>
2024-07-14 19:39:10 +0300

    finish homework30

--- homework30/src/main/java/com/teachmeskills/Main.java
+++ homework30/src/main/java/com/teachmeskills/Main.java
@@ -1,11 +1,16 @@
 package com.teachmeskills;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author zafarzhon
  * @link https://github.com/zafarzhon
  */
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("app.xml");
+        DbService bean = context.getBean(DbService.class);
     }
 }
