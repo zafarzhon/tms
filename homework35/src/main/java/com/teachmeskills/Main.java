50bf344509da991918be75486557faad0fb288b9
zafarzhon <odilovzafarzhon@gmail.com>
2024-09-20 16:51:48 +0300

    add module homework35

---
+++ homework35/src/main/java/com/teachmeskills/Main.java
@@ -0,0 +1,12 @@
+package com.teachmeskills;
+
+/**
+ * @author zafarzhon
+ * @link <a href="https://github.com/zafarzhon">github</a>
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
