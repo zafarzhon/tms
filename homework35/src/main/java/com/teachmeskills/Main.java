d6145af0f9224ac3f0b420c87dd8813b4f0f4e24
zafarzhon <odilovzafarzhon@gmail.com>
2024-09-20 19:03:41 +0300

    finish homework35

--- homework35/src/main/java/com/teachmeskills/Main.java
+++ homework35/src/main/java/com/teachmeskills/Main.java
@@ -1,12 +1,31 @@
 package com.teachmeskills;
 
+import com.teachmeskills.config.AppConfig;
+import com.teachmeskills.entities.Person;
+import com.teachmeskills.entities.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.UUID;
+
 /**
  * @author zafarzhon
  * @link <a href="https://github.com/zafarzhon">github</a>
  */
 
 public class Main {
+    private static SessionFactory sessionFactory = new AppConfig().sessionFactory();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Person john = new Person(UUID.randomUUID(),"John", 2000, Role.ADMIN);
+        Person jane = new Person(UUID.randomUUID(),"Jane", 2000, Role.USER);
+        session.save(john);
+        session.save(jane);
+        transaction.commit();
+        session.close();
+
     }
+
 }
