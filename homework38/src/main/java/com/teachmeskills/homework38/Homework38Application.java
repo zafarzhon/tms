9b4fe55faef4d22b0819df3da654ff51cf246d77
zafarzhon <odilovzafarzhon@gmail.com>
2024-09-25 00:26:41 +0300

    add module homework38

---
+++ homework38/src/main/java/com/teachmeskills/homework38/Homework38Application.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.homework38;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework38Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework38Application.class, args);
+	}
+
+}
---
+++ homework38/src/test/java/com/teachmeskills/homework38/Homework38ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.homework38;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework38ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
