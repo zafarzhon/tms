8adfec72741d4e9b0ede6ab0a617d7d196579449
zafarzhon <odilovzafarzhon@gmail.com>
2024-09-26 15:57:22 +0300

    add module homework39

---
+++ homework39/src/main/java/com/teachmeskills/homework39/Homework39Application.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.homework39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework39Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework39Application.class, args);
+    }
+
+}
---
+++ homework39/src/test/java/com/teachmeskills/homework39/Homework39ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.homework39;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework39ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
