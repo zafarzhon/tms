3f98c854c8bb3a4ea4bb3d1eebc85f01fe89026a
zafarzhon <odilovzafarzhon@gmail.com>
2024-09-27 16:27:49 +0300

    add module homework40

---
+++ homework40/src/main/java/com/teachmeskills/shop/Homework40Application.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework40Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework40Application.class, args);
+    }
+
+}
---
+++ homework40/src/test/java/com/teachmeskills/shop/Homework40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework40ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
