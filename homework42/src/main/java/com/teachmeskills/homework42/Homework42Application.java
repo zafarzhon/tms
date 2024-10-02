13897b86b01d02f1241c00c6b43cd9a95d51970d
zafarzhon <odilovzafarzhon@gmail.com>
2024-10-02 17:01:28 +0300

    add module homework42

---
+++ homework42/src/main/java/com/teachmeskills/homework42/Homework42Application.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.homework42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework42Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework42Application.class, args);
+    }
+
+}
---
+++ homework42/src/test/java/com/teachmeskills/homework42/Homework42ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.homework42;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework42ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
