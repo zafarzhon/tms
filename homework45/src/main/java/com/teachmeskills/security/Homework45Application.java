faa4e35549c015313d75f532435ffe8f9fd98de0
zafarzhon <odilovzafarzhon@gmail.com>
2024-10-06 22:37:12 +0300

    add module homework45

---
+++ homework45/src/main/java/com/teachmeskills/security/Homework45Application.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework45Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework45Application.class, args);
+    }
+
+}
---
+++ homework45/src/test/java/com/teachmeskills/security/Homework45ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework45ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
