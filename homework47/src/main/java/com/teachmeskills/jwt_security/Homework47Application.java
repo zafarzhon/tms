ddee11a37fb0ea7b3cdaa78cde47a2090058e7fb
zafarzhon <odilovzafarzhon@gmail.com>
2024-10-10 23:27:54 +0300

    add module homework47

---
+++ homework47/src/main/java/com/teachmeskills/jwt_security/Homework47Application.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.jwt_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework47Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework47Application.class, args);
+    }
+
+}
---
+++ homework47/src/test/java/com/teachmeskills/jwt_security/Homework47ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.jwt_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework47ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
