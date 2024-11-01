6a0a3c3d755fb92cc3bd61c9e157b8043e6152ee
zafarzhon <odilovzafarzhon@gmail.com>
2024-11-01 14:02:52 +0300

    add module homework51

---
+++ homework51/src/main/java/com/example/homework51/Homework51Application.java
@@ -0,0 +1,13 @@
+package com.example.homework51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework51Application.class, args);
+    }
+
+}
